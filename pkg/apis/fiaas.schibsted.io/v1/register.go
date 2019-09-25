7b0adcbc05740a6e7442bc03fac83b4e355a4068
Marta Paciorkowska <marta.paciorkowska@finn.no>
2019-09-25 13:58:21 +0200

    We have to add our ApplicationStatus top-level types to the scheme, too

--- pkg/apis/fiaas.schibsted.io/v1/register.go
+++ pkg/apis/fiaas.schibsted.io/v1/register.go
@@ -58,6 +58,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&Application{},
 		&ApplicationList{},
+		&ApplicationStatus{},
+		&ApplicationStatusList{},
 	)
 
 	scheme.AddKnownTypes(SchemeGroupVersion,
