0be3570ed606dff0ccec2a8b2ead2136dac98a06
Marta Paciorkowska <marta.paciorkowska@finn.no>
2019-09-30 15:23:56 +0200

    Consistent versioning for kubernetes 1.15.0 (#6)
    * Adds a 'go test' to CI
    
    * Uses apimachinery and client-go with the same version tag, kubernetes-1.15.0
    
    * update-codegen should use the same version tag as other packages: 1.15.0
    
    * Semaphore needs to get go dependencies before running tests
    
    * Remove tab from semaphore.yml
    
    * semaphore.yml: Explicitly sets GO111MODULE=on since we're inside GOPATH

--- pkg/client/clientset/versioned/typed/fiaas.schibsted.io/v1/fiaas.schibsted.io_client.go
+++ pkg/client/clientset/versioned/typed/fiaas.schibsted.io/v1/fiaas.schibsted.io_client.go
@@ -5,7 +5,6 @@ package v1
 import (
 	v1 "github.com/fiaas/fiaas-go-client/pkg/apis/fiaas.schibsted.io/v1"
 	"github.com/fiaas/fiaas-go-client/pkg/client/clientset/versioned/scheme"
-	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -60,7 +59,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
